a3e789e6fa2caa33613294350dc8e9276bbacb33
Pedro_Macbook <pedrocarregado15@gmail.com>
2025-10-21 22:28:03 +0100

    pagina do produto

--- scripts/script_conquistas.js
+++ scripts/script_conquistas.js
@@ -1,13 +1,14 @@
 fetch('../scripts/conquistas.json')
   .then(res => res.json())
   .then(products => {
-    const grid = document.getElementById('new-arrivals-grid');
+    const grid = document.getElementById('product-display-grid');
 
     products.forEach(p => {
       const card = document.createElement('div');
-      card.className = 'new-arrival-card';
+      card.className = 'product-card';
 
       card.innerHTML = `
+        <a href="../pages/produto.html?id=${p.id}" class="product-link">
         ${p.new ? '<span class="badge-new">Novo</span>' : ''}
         <img src="${p.image}" alt="${p.name}" />
         <h4>${p.name}</h4>
