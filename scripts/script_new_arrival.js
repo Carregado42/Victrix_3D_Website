cb552d51564e1b19a8fd5a664a4309e9cf4f50d1
Pedro <pedrocarregado15@gmail.com>
2025-10-21 16:51:51 +0100

    Pagina das Conquistas

---
+++ scripts/script_new_arrival.js
@@ -0,0 +1,22 @@
+fetch('../scripts/new_products.json')
+  .then(res => res.json())
+  .then(products => {
+    const grid = document.getElementById('new-arrivals-grid');
+
+    products.forEach(p => {
+      const card = document.createElement('div');
+      card.className = 'new-arrival-card';
+
+      card.innerHTML = `
+        ${p.new ? '<span class="badge-new">Novo</span>' : ''}
+        <img src="${p.image}" alt="${p.name}" />
+        <h4>${p.name}</h4>
+        <div class="price">${p.price}</div>
+      `;
+
+      grid.appendChild(card);
+    });
+  })
+  .catch(err => console.error('Erro ao carregar produtos:', err));
+
+
